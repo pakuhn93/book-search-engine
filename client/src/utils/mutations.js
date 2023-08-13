067c5643c548375fd2f58cb9a1bdb1c12fcc9f8e
pakuhn93 <pakuhn93@gmail.com>
2023-08-13 16:24:00 -0700

    added some comment blocks to specify what i need to write in client-side data management

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,4 +2,26 @@
 // The code that goes here is basically the same as what goes into the Apollo Client when running queries through the GraphQL endpoint.
 // the coding convention in this file should be ALL_CAPS and their names should resemble their function.
 
+
 // import { gql } from '@apollo/client';
+
+/*
+
+export const CREATE_USER = gql`
+`;
+
+*/
+
+/*
+
+export const SAVE_BOOK = gql`
+`;
+
+*/
+
+/*
+
+export const DELETE_BOOK = gql`
+`;
+
+*/
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,3 +4,10 @@
 
 
 // import { gql } from '@apollo/client';
+
+/* export const QUERY_USER = gql`
+`;
+
+
+
+*/
