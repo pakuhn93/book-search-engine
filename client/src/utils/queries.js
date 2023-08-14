27e980a4d361552d733b9386b7bb80aebca313db
pakuhn93 <pakuhn93@gmail.com>
2023-08-14 14:45:37 -0700

    adjusted queries in client, had one more field than needed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,18 +8,17 @@ import { gql } from '@apollo/client';
 export const GET_ME = gql`
     query Query {
         me {
-        _id
-        username
-        email
-        password
-        savedBooks {
-            bookId
-            authors
-            description
-            image
-            link
-            title
-        }
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                image
+                link
+                title
+            }
         }
     }
 `;
