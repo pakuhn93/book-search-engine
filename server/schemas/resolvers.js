2ba285f5572c99563f6a25b1fa4a861e530e1e11
pakuhn93 <pakuhn93@gmail.com>
2023-08-14 03:30:33 -0700

    finished up back-end typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,78 @@
 // This file serves as the functionality for our CRUD
 const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 // resolvers is just a fancy object that holds properties that run our queries / mutations
 const resolvers = {
-    // Query: {
-    //     // get one User
-    // },
+    Query: {
+        // get one User | instructions specify to call this 'me'
+        me: async (parent, args, context) => {
+            // we define our context at the start of our server through expressMiddleware, which then goes to our auth.js (server-side)
+            // in these cases, we're authenticating our Users
+            if (context.user){
+                const userData = await User.findOne({ _id: context.user._id });
 
-    // Mutation: {
-    //     // create one User
-    //     // save book | updates User books property
-    //     // delete book | delete one from User books property
-    // }
+                return userData;
+            }
+            // throw an Auth error if the above condtional is false
+            throw AuthenticationError;
+        }
+    },
+
+    Mutation: {
+        // create one User
+        // always need to put parent as the first argument with our resolver functions
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email }); // need to send as an object when using findOne()
+
+            // check if user exists
+            if(!user){ throw AuthenticationError };
+
+            // check if correct password
+            const correctPass = await user.isCorrectPasssword(password);
+            if(!correctPass){ throw AuthenticationError };
+
+            // assign a session token to the user
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        // save book | updates User books property
+        saveBook: async (parent, args, context) => {
+            if(context.user){
+                const updatedUser = await User.findByIdAndUpdate(
+                      { // 1st parameter is for the User's _id property, to find the right user to put the book into
+                        _id: context.user._id
+                      }, // push the new book into the savedBooks property on the User model
+                      { $push: { savedBooks: args }},
+                      // the 'new' property ensures that the user is returned the new, updated, data, rather than the old data
+                      { new: true }
+                    );
+                return updatedUser;
+            }
+            // if our conditional above isn't true, throw an Auth error
+            throw AuthenticationError;
+        },
+
+        // delete book | delete one from User books property
+        removeBook: async (parent, { bookId }, context) => {
+            if(context.user){
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: {bookId }}},
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        }
+    }
 };
-// somewhere in resolvers, we will need to do User Authentication
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,38 @@
 // This file includes what objects can be queried and mutated, as well as defining what properties we are allowed to access on these objects.
 
 const typeDefs = `
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
 
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: [User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
